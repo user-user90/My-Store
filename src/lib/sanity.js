e0280984be74ae67bc2b1f08e030310ff10b60ab
user-user90 <wissalinas6@gmail.com>
2026-03-11 00:27:36 +0000

    Mise à jour du design et ajout des notifications toast

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -4,5 +4,5 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // استخدام المتغير البيئي
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2026-02-22", 
-  useCdn: true, 
+  useCdn: false, 
 });
